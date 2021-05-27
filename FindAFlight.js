54acac0eb83369e8c334427624445c0ae8f769c4
yesuch-kore <yesu.chiratanagandla@kore.com>
2021-05-27 17:16:56 +0530

    Updated reference logic for FindAFlight and exampleWithExternalNLPEngine modules with the latest API implementation

--- FindAFlight.js
+++ FindAFlight.js
@@ -1,43 +1,80 @@
-var botId = "st-a51421b1-39c9-5fb0-ba02-70f4455a7c5e";
-var botName = "Flight search";
+var botId = "st-48d9b4a2-b544-5cda-8313-31fbe0e3a8b9";
+var botName = "Flight Search";
 var sdk            = require("./lib/sdk");
 var Promise        = sdk.Promise;
 var request        = require("request");
-var config         = require("./config");
-//var mockServiceUrl = config.examples.mockServicesHost + '/cabbot';
 
+var findFlightServiceUrl = 'https://test.api.amadeus.com/v2/shopping/flight-offers';
+var findAirportServiceUrl = 'https://test.api.amadeus.com/v1/reference-data/locations';
+var getTokenUrl = 'https://test.api.amadeus.com/v1/security/oauth2/token';
+var amdAccessToken;
 
-var amdApiKey = "<%amdApiKey%>";
-
-//Make request to service app
-function findFlights(origin, destination, departureDate) {
-	var serviceUrl = 'http://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?apikey='+amdApiKey+'&number_of_results=5';
+// Make a request to get the access token
+function getAccessToken(clientId, clientSecret) {
+    var url = getTokenUrl;
     return new Promise(function(resolve, reject) {
-        request({
-            url: serviceUrl+'&origin='+origin+'&destination='+destination+'&departure_date='+departureDate,
-            method: 'get',
-        }, function(err, res) {
-            if (err) {
+        const requestOptions = {
+            url: url,
+            method: 'post',
+            headers: {
+                'Content-type': 'application/x-www-form-urlencoded'
+            },
+            form: {
+                client_id: clientId,
+                client_secret: clientSecret,
+                grant_type: 'client_credentials'
+            }
+        };
+        request(requestOptions, function(err, res) {
+            if(err) {
                 return reject(err);
             }
-	        resolve(JSON.parse(res.body));
+            // console.log(res)
+            var temp = JSON.parse(res.body);
+            var token = temp['access_token'];
+            resolve(token);
+        });
+    });
+}
+
+//Make request to service app to get the flight between source and destination
+function findFlights(origin, destination, departureDate, currency) {
+    var url = findFlightServiceUrl+'?originLocationCode='+origin+'&destinationLocationCode='+destination+'&departureDate='+departureDate+'&adults=1&max=5&currencyCode='+currency;
+    return new Promise(function(resolve, reject) {
+        const requestOptions = {
+            url: url,
+            method: 'get',
+            headers: {
+                'Authorization': 'Bearer ' + amdAccessToken
+            }
+        };
+        request(requestOptions, function(err, response) {
+            if(err) {
+                return reject(err);
+            }
+            resolve(JSON.parse(response.body));
         });
     });
 }
 
 
 //Make request to service app
-function findAirports(searchTerm) {
-    var airportsUrl="http://api.sandbox.amadeus.com/v1.2/airports/autocomplete?apikey="+amdApiKey;
+async function findAirports(searchTerm) {
+    var url = findAirportServiceUrl+"?keyword="+searchTerm+"&subType=AIRPORT";
     return new Promise(function(resolve, reject) {
-        request({
-            url: airportsUrl+"&term="+searchTerm,
+        const requestOptions = {
+            url: url,
             method: 'get',
-        }, function(err, res) {
-            if (err) {
+            headers: {
+                'Authorization': 'Bearer ' + amdAccessToken
+            }
+        };
+        request(requestOptions, function(err, response) {
+            if(err) {
+                console.log(err);
                 return reject(err);
             }
-            resolve(JSON.parse(res.body));
+            resolve(JSON.parse(response.body));
         });
     });
 }
@@ -53,36 +90,36 @@ module.exports = {
     on_bot_message  : function(requestId, data, callback) {
         sdk.sendUserMessage(data, callback);
     },
-    on_webhook      : function(requestId, data, componentName, callback) {
+    on_webhook      : async function(requestId, data, componentName, callback) 
+    {
         var context = data.context;
-	    if (componentName === 'FlightsInfo') {
-		  var origin = context.entities.Source;
-          var destination = context.entities.Dest;
-          var departureDate = context.entities.Date;
-          findFlights(origin,destination,departureDate)
-			.then(function(flightResults) {
-                data.context.flightResults = flightResults;
-				callback(null, data);
-			});
-	    } else if(componentName === 'GetSourceAirports'){
-
-             var searchTerm = context.entities.SourceName;
-             findAirports(searchTerm)
-            .then(function(airportResults) {
-                data.context.sourceAirports = airportResults;
-                callback(null, data);
-            });
-        } else if(componentName === 'GetDestAirports'){
-           var searchTerm = context.entities.DestName;
-             findAirports(searchTerm)
-            .then(function(airportResults) {
-                data.context.destAirports = airportResults;
-                callback(null, data);
-            });
-        }
-        
-    },
-    on_agent_transfer : function(requestId, data, callback){
-        return callback(null, data);
+        var currency = context.session.BotUserSession.currency;
+        getAccessToken(context.session.BotUserSession.clientId, context.session.BotUserSession.clientSecret).then(function(token) {
+            amdAccessToken = token;
+            if (componentName === 'FlightsInfo') {
+                var origin = context.entities.Source;
+                var destination = context.entities.Dest;
+                var departureDate = context.entities.Date;
+                findFlights(origin,destination,departureDate,currency)
+                .then(function(flightResults) {
+                    data.context.flightResults = flightResults;
+                    callback(null, data);
+                });
+            } else if(componentName === 'GetSourceAirports'){
+                var searchTerm = context.entities.SourceName;
+                findAirports(searchTerm)
+                .then(function(airportResults) {
+                    data.context.sourceAirports = airportResults;
+                    callback(null, data);
+                });
+            } else if(componentName === 'GetDestAirports'){
+                var searchTerm = context.entities.DestName;
+                findAirports(searchTerm)
+                .then(function(airportResults) {
+                    data.context.destAirports = airportResults;
+                    callback(null, data);
+                });
+            }
+        });
     }
 };
--- exampleWithExternalNLPEngine.js
+++ exampleWithExternalNLPEngine.js
@@ -6,170 +6,196 @@
  * for details on how to setup the Bot and run this example
  */
 
-var botId   = "st-******************"; //BotID from Kore.ai - Refer to settings section
-var botName = "**********";  //botname from Kore.ai
-var sdk     = require("./lib/sdk");
-var config  = require("./config");
-var request = require('request-promise');
-var Promise = sdk.Promise;
-
-
-//Api.ai funtion call for intent recognization
-function getIntentFromApiai(message){
-   return new Promise(function(resolve, reject) {
-        request({
-	    // Update the below url with the api.ai bot configuration.
-            url: 'https://api.api.ai/api/query?v=20150910&query='+message+'&lang=en&sessionId=72119261-b1c2-4996-84dd-d874d7754adc',
-            method: 'GET',
-            headers: {
-                    // authorization bearer token from Api.ai
-                    'Authorization': 'Bearer ****************'
-            }
-        }, function(error, res) {
-            if (error || !res.body) {
-                reject({error:error});
-            }else{
-       resolve(JSON.parse(res.body));
-     }
-        });
-    });
-}
-
-
-//Luis.ai function for entity recognization
-function getEntitiesFromluis(message){
-   return new Promise(function(resolve, reject) {
-        request({
-	    // Update the below url with the Luis.ai bot configuration.
-            url: 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/12730170-ef21-4047-9900-03de0bfdf14a?subscription-key=c063935b88e84bad98e40d6fee3f03bd&verbose=true&timezoneOffset=0&q='+message,
-            method: 'GET',
-            headers: {
-                //Authorization bearer token from Luis.ai
-                'Authorization': 'Bearer *******************'
-            }
-        }, function(error, res) {
-            if (error || !res.body) {
-                reject({error:error});
-            }else{
-       resolve(JSON.parse(res.body));
-     }
-        });
-    });
-}
-
-//Send welcome message to the user, when they interact with the Bot for the first time
-function sendWelcomeMessage(data){
-   data.message = "Hello, I can help you with searching hotels near your city of interest";
-}
-
+ var sdk     = require("./lib/sdk");
+ var config  = require("./config");
+ var botId   = config.credentials.botId; //BotID from Kore.ai - Refer to settings section
+ var botName = config.credentials.botName;  //botname from Kore.ai
+ var externalNLPConfig = require('./externalNLPConfig.json');
+ var request = require('request-promise');
+ const dialogflow = require('@google-cloud/dialogflow');
+ const uuid = require('uuid');
+ var Promise = sdk.Promise;
  
- module.exports = {
-
-    botId   : botId,
-    botName : botName,
-    
-    //on_user_message handler
-    on_user_message : function(requestId, data, callback) {
-      
-      var message = (data.message).trim();
-      var re = new RegExp(".*cab.*"); //handle specific messages for example cab related info
-      
-      //Check if first time interaction, if yes, send out the welcome message
-      if(data.context.session.BotUserSession 
-          && !data.context.session.BotUserSession.isFirstMessage) { 
-          
-          data.context.session.BotUserSession.isFirstMessage = true;
-          sendWelcomeMessage(data);
-          sdk.sendUserMessage(data,callback);
-          
+ const languageCode = externalNLPConfig.dialogFlow.languageCode;
+ const projectId = externalNLPConfig.dialogFlow.projectId;
+ const sessionClient = new dialogflow.SessionsClient({
+     keyFilename: externalNLPConfig.dialogFlow.serviceFileName
+ });
+ const sessionId = uuid.v4();
+ const sessionPath = sessionClient.projectAgentSessionPath(projectId, sessionId);
+ 
+ //Dialog Flow sdk call for intent recognization
+ function getIntentFromDialogFlow(message){
+     return new Promise(function(resolve, reject) {
+         const request = {
+             session: sessionPath,
+             queryInput: {
+                 text: {
+                     text: message,
+                     languageCode: languageCode
+                 }
+             }
+         };
+     
+         sessionClient.detectIntent(request, function(err, response) {
+             if(err) {
+                 reject({ error: err });
+             }
+             resolve(response);
+         });
+     });
+ }
+ 
+ 
+ //Luis.ai function for entity recognization
+ function getEntitiesFromluis(message){
+    return new Promise(function(resolve, reject) {
+         request({
+         // Update the below url with the Luis.ai bot configuration.
+             url: externalNLPConfig.luis.baseUrl + 'luis/prediction/v3.0/apps/'+ externalNLPConfig.luis.appId+'/slots/'+ externalNLPConfig.luis.slot+'/predict?subscription-key='+externalNLPConfig.luis.subscriptionKey+'&verbose=true&show-all-intents=true&log=true&query='+message,
+             method: 'GET'
+         }, function(error, res) {
+             if (error || !res.body) {
+                 reject({error:error});
+             }else{
+                 resolve(JSON.parse(res.body));
       }
-
-      getIntentFromApiai(message).then(function(intentResponse){   
+         });
+     });
+ }
+ 
+ //Send welcome message to the user, when they interact with the Bot for the first time
+ function sendWelcomeMessage(data){
+    data.message = "Hello, I can help you with searching hotels near your city of interest";
+ }
+ 
+  
+  module.exports = {
+ 
+     botId   : botId,
+     botName : botName,
+     
+     //on_user_message handler
+     on_user_message : function(requestId, data, callback) {
+       var message = (data.message).trim();
+       var re = new RegExp(".*cab.*"); //handle specific messages for example cab related info
+       
+       //Check if first time interaction, if yes, send out the welcome message
+       if(data.context.session.BotUserSession && !data.context.session.BotUserSession.isFirstMessage) { 
+           
+           data.context.session.BotUserSession.isFirstMessage = true;
+           sendWelcomeMessage(data);
+           sdk.sendUserMessage(data,callback);
+           
+       }
+ 
+       getIntentFromDialogFlow(message).then(function(intentResponse) {   
+         if(intentResponse.queryResult.intentDetectionConfidence > 0) {      //if intent recognized from dialog flow
+             
+           var intent = intentResponse.queryResult.intent.displayName;
+           console.log(intent);
+           data.message = 'Intent identified from Dialog Flow is '+ intent;
+ 
+         //   if (intentResponse.result.metadata && intentResponse.result.metadata.intentName) {
+         //       intent = intentResponse.result.metadata.intentName
+         //   }
+ 
+           sdk.sendUserMessage(data,callback);
+           
+           //Lets try to get entities from another NLP service, Luis.ai
+           getEntitiesFromluis(message).then(function(entitiesResponse) {
+ 
+               data.metaInfo = {
+                   'intentInfo' : {
+                     //   'intent' : intent
+                   }
+               };
+ 
+               if('entities' in entitiesResponse.prediction) {      //if entities recognised from luis or not
+               
+                 var entities = entitiesResponse.prediction.entities;
+                 var date;
+                 var city;
+                 if(entities.datetimeV2 !== undefined && entities.datetimeV2.length > 0) {
+                     date = entities.datetimeV2[0].values[0].resolution[0].value;
+                 }
+ 
+                 if(entities.City !== undefined && entities.City.length > 0) {
+                     city = entities.City[0];
+                 }
+ 
+                 if(date !== undefined || city !== undefined) {
+                     var temp = '';
+                     var entityToKoreNL = {}; // entityToKoreNL object should contain the keys as per the Kore entity recognition documentation.
+                     if(date !== undefined) {
+                         temp = temp + date + ' : ';
+                         entityToKoreNL['date'] = date;
+                     }
+ 
+                     if(city !== undefined) {
+                         temp = temp + city;
+                         entityToKoreNL['City'] = city;
+                     }
+ 
+                     data.message = 'Entities identified from luis.ai is '+ temp;
+                     
+                     // Uncomment the following to see a message on chat window, on what intents and entities 
+                     // are identified on Api.ai and from Luis.ai
+                 
+                     sdk.sendUserMessage(data,callback);
+                 
+                     data.message = 'Search hotels'; // Here we are setting the message to match to task name on Kore.ai, this message will be ignored if intent is passed externally.
+                     
+                     data.metaInfo.intentInfo.entities = entityToKoreNL;
+ 
+                     // Now lets send a message to Kore.ai Bot, with data that says, the intent is already recognized
+                     // and the entities are also set into the context
+                     return sdk.sendBotMessage(data,callback);
+                 } else {
+                     data.message = 'No Entities identified';
+                     sdk.sendUserMessage(data,callback);
+                     data.message = 'Search hotels';
+                     return sdk.sendBotMessage(data,callback);
+                 }
+               } else {  //Intent recognized from Dialog Flow, but the entity not recognized from Luis.ai
+                 data.message = 'Search hotels';
+ 
+                 // Now lets send a message to Kore.ai Bot, with data that says, the intent is already recognized
+                 // Kore.ai Bot should prompt for the entity in this case, as we did not set the entity value into context
+               
+                 return sdk.sendBotMessage(data,callback);
+               }
+           });
+       } else if(re.test(message)){
+           
+           // Send out a custom message, if user says that "I want to book a cab"
+           data.message = 'You can book a cab once you reach the hotel';
+           return sdk.sendUserMessage(data,callback);
+       
+       }else{
+           return sdk.sendBotMessage(data, callback);
+       }
+     });
+     },
+ 
+     //on_bot_message handler
+     on_bot_message  : function(requestId, data, callback) {
+         // In case of intent is not identified by Kore NL, 'intentMatched' flag will false. (data._originalPayload.metaInfo.intentInfo.intentMatched)  
+         return sdk.sendUserMessage(data, callback);
+     },
     
-        if(intentResponse.result.score > 0){      //if intent recognized from api.ai
-      
-          var intent = intentResponse.result.action;
-          data.message = 'Intent identified from api.ai is '+ intent;
-
-          if (intentResponse.result.metadata && intentResponse.result.metadata.intentName) {
-              intent = intentResponse.result.metadata.intentName
-          }
-
-          //sdk.sendUserMessage(data,callback);
-          
-          //Lets try to get entities from another NLP service, Luis.ai
-          getEntitiesFromluis(message).then(function(entitiesResponse){
-
-              data.metaInfo = {
-                  'intentInfo' : {
-                      'intent' : intent
-                  }
-              };
-
-              if(entitiesResponse.entities.length > 0){      //if entities recognised from luis or not
-              
-                var entities = entitiesResponse.entities;
-                data.message = 'Entities identified from luis.ai is '+ entities[0].entity;
-              
-                // Uncomment the following to see a message on chat window, on what intents and entities 
-                // are identified on Api.ai and from Luis.ai
-              
-                // sdk.sendUserMessage(data,callback);
-              
-                data.message = 'Search hotels'; // Here we are setting the message to match to task name on Kore.ai, this message will be ignored if intent is passed externally.
-                var entities = entitiesResponse.entities;
-                if(entities[0].type === 'builtin.geography.city') {
-                    var entityToKoreNL = { // entityToKoreNL object should contain the keys as per the Kore entity recognition documentation.
-                        'City': entities[0].entity
-                    };
-                    data.metaInfo.intentInfo.entities = entityToKoreNL;
-                }
-
-                // Now lets send a message to Kore.ai Bot, with data that says, the intent is already recognized
-                // and the entities are also set into the context
-              
-                return sdk.sendBotMessage(data,callback);
-            
-              }else{  //Intent recognized from Api.ai, but the entity not recognized from Luis.ai
-              
-                data.message = 'Search hotels';
-
-                // Now lets send a message to Kore.ai Bot, with data that says, the intent is already recognized
-                // Kore.ai Bot should prompt for the entity in this case, as we did not set the entity value into context
-              
-                return sdk.sendBotMessage(data,callback);
-              }
-          });
-      }else if(re.test(message)){
-          
-          // Send out a custom message, if user says that "I want to book a cab"
-          data.message = 'You can book a cab once you reach the hotel';
-          return sdk.sendUserMessage(data,callback);
-      
-      }else{
-          return sdk.sendBotMessage(data, callback);
-      }
-    });
-    },
-
-    //on_bot_message handler
-    on_bot_message  : function(requestId, data, callback) {
-        // In case of intent is not identified by Kore NL, 'intentMatched' flag will false. (data._originalPayload.metaInfo.intentInfo.intentMatched)  
-        return sdk.sendUserMessage(data, callback);
-    },
-   
-    //on_webhook handler
-    on_webhook      : function(requestId, data, componentName, callback) {
-        if (componentName === 'sendResponse') {
-          var hotels = {
-            "hotels":[
-                  "Taj Banjara",
-                  "Novotel"
-            ]
-          };
-          data.context.hotelResults = hotels;
-          callback(null,data);
-        }
-    }
-        
-};
+     //on_webhook handler
+     on_webhook      : function(requestId, data, componentName, callback) {
+         if (componentName === 'sendResponse') {
+           var hotels = {
+             "hotels":[
+                   "Taj Banjara",
+                   "Novotel"
+             ]
+           };
+           data.context.hotelResults = hotels;
+           callback(null,data);
+         }
+     }
+         
+ };
